9c027e6e5017937d8a2541ef8aef9af7a506f821
wangji <983433479@qq.com>
2021-04-18 22:17:07 +0800

    demo project

---
+++ dingtalk-robot-logback-demo/src/main/java/com/github/wangji92/logbackappenddemo/DingtalkRobotLogbackDemoApplication.java
@@ -0,0 +1,16 @@
+package com.github.wangji92.logbackappenddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 汪小哥
+ */
+@SpringBootApplication
+public class DingtalkRobotLogbackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DingtalkRobotLogbackDemoApplication.class, args);
+    }
+
+}
---
+++ dingtalk-robot-logback-demo/src/test/java/com/github/wangji92/logbackappenddemo/DingtalkRobotLogbackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wangji92.logbackappenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingtalkRobotLogbackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
